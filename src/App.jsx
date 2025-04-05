251e99f3f126de5d939b21fc23e7197ceee42ccf
Pujan Neupane <pujanneupane@example.com>
2025-04-05 16:41:33 +0545

    removed the name of the about

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,18 @@ import { useState } from "react";
 import "./App.css";
 import { Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/top" element={<About />} />
-      </Routes>
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/top" element={<About />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
